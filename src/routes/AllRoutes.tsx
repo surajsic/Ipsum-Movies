af991c2d74bfa2ee24bbe381261f3f098fb7a236
surajsic28@gmail.com <142887205+surajsic@users.noreply.github.com>
2024-12-09 18:13:36 +0530

    update

--- src/routes/AllRoutes.tsx
+++ src/routes/AllRoutes.tsx
@@ -9,7 +9,7 @@ import { Route, Routes } from "react-router"
 const AllRoutes = () => {
   return (
     <Routes>
-        {<Route path="/" element={<Trending />}/>}
+        <Route path="/" element={<Trending />}/>
         <Route path="/movies" element={<MovieList />}/>
         <Route path="/tvshows" element={<TvShowList />}/>
         <Route path="/search/:searchName/" element={<SearchList />}/>
